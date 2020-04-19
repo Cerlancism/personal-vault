cd1d761d3045820e2a4c63e0e48a40d3f662ec99
Cerlancism <scheduledx@gmail.com>
2020-04-20 02:55:20 +0800

    Add key validator

--- index.js
+++ index.js
@@ -6,3 +6,18 @@ const express = require('express')
 const { generateRandomHex, generateHash, getSaltedHash, encrypt, decrypt } = require('./crypto-utils')
 
 const app = express()
+
+const vaultFiles = fs.readdirSync("./vault")
+
+/**
+ * 
+ * @param {string} target 
+ * @param {string} key 
+ */
+function isValidKey(target, key)
+{
+    const salted = getSaltedHash(target, key)
+    return vaultFiles.some(x => x == salted)
+}
+
+
